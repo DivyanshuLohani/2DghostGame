66c48eeb9f5bc14ed098c65f9c09b743419b0c7f
Divyanshu Lohani <divyanshulohani2305@gmail.com>
2023-10-19 13:58:04 +0530

    add hiscore

--- utils.py
+++ utils.py
@@ -6,6 +6,42 @@ import pygame
 pygame.font.init()
 
 
+def check_highscore(force=False):
+    # Check if file exists
+    # Windows only
+    base = os.getenv("APPDATA")
+    path = os.path.join(base, "GhostGame")
+    if not os.path.exists(path):
+        os.makedirs(path)
+    file_path = os.path.join(path, "score.dat")
+    if force or not os.path.exists(file_path):
+        with open(file_path, "w") as f:
+            f.write("0")
+    return file_path
+
+
+def read_hiscore():
+    file_path = check_highscore()
+    with open(file_path, "r") as f:
+        score = f.read()
+        if not score.isdigit() or "." in score:  # Check float
+            score = 0
+            # File is couupt
+            check_highscore(True)
+        else:
+            score = int(score)
+
+    return score
+
+
+def save_hiscore(score):
+    file_path = check_highscore()
+    with open(file_path, "w") as f:
+        f.write(str(score))
+
+    return score
+
+
 def get_font(size):
     return pygame.font.Font("assets/Roboto-Black.ttf", size)
 
